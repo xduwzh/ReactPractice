42bd87f3db7756501a7604469bae80baa8bbafff
ZhenghangWu <wzh4396@outlook.com>
2025-07-30 12:43:15 +0200

    Custom hook; server data; comment item component

--- src/commentArea.js
+++ src/commentArea.js
@@ -1,49 +1,12 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./commentArea.scss";
 import avatar from "./images/bozai.png";
 import _ from "lodash";
 import classNames from "classnames";
+import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
+import axios from "axios";
 
-// comment list
-const defaultList = [
-  {
-    // 评论id
-    rpid: 3,
-    // 用户信息
-    user: {
-      uid: "13258165",
-      avatar: "",
-      uname: "Jay Chou",
-    },
-    // 评论内容
-    content: "哎哟，不错哦",
-    // 评论时间
-    ctime: "10-18 08:15",
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: "36080105",
-      avatar: "",
-      uname: "许嵩",
-    },
-    content: "我寻你千百度 日出到迟暮",
-    ctime: "11-13 11:29",
-    like: 100,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: "30009257",
-      avatar,
-      uname: "wzh",
-    },
-    content: "Ars longa, vita brevis.",
-    ctime: "10-19 09:00",
-    like: 999,
-  },
-];
 // logined user
 const user = {
   uid: "30009257",
@@ -57,10 +20,56 @@ const tabs = [
   { type: "time", text: "Latest" },
 ];
 
-const CommentArea = () => {
-  const [commentList, setCommentList] = useState(
-    _.orderBy(defaultList, "like", "desc")
+function useGetList() {
+  const [commentList, setCommentList] = useState([]);
+  useEffect(() => {
+    async function getList() {
+      const res = await axios.get("http://localhost:3004/list");
+      setCommentList(res.data);
+    }
+    getList();
+  }, []);
+
+  return {
+    commentList,
+    setCommentList,
+  };
+}
+
+function CommentItem({ item, onDel }) {
+  return (
+    <div key={item.rpid} className="reply-item">
+      <div className="root-reply-avatar">
+        <div className="bili-avatar">
+          <img className="bili-avatar-img" alt="" src={item.user.avatar} />
+        </div>
+      </div>
+
+      <div className="content-wrap">
+        <div className="user-info">
+          <div className="user-name">{item.user.uname}</div>
+        </div>
+        <div className="root-reply">
+          <span className="reply-content">{item.content}</span>
+          <div className="reply-info">
+            <span className="reply-time">{item.ctime}</span>
+            <span className="reply-time">like: {item.like}</span>
+            {user.uid === item.user.uid && (
+              <span className="delete-btn" onClick={() => onDel(item.rpid)}>
+                Delete
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
+}
+const CommentArea = () => {
+  //   const [commentList, setCommentList] = useState(
+  //     _.orderBy(defaultList, "like", "desc")
+  //   );
+  const { commentList, setCommentList } = useGetList();
   const handleDel = (id) => {
     console.log(id);
     setCommentList(commentList.filter((item) => item.rpid !== id));
@@ -75,6 +84,29 @@ const CommentArea = () => {
     } else {
       setCommentList(_.orderBy(commentList, "ctime", "desc"));
     }
+  };
+
+  const [content, setContent] = useState("");
+  const inputRef = useRef(null);
+  const handlePublish = () => {
+    setCommentList([
+      ...commentList,
+      {
+        rpid: uuidv4(),
+        user: {
+          uid: "30009257",
+          avatar,
+          uname: "wzh",
+        },
+        content: content,
+        ctime: dayjs(new Date()).format("MM-DD hh:mm"),
+        like: 1999,
+      },
+    ]);
+    // empty content after sending
+    setContent("");
+    // refocus
+    inputRef.current.focus();
   };
 
   return (
@@ -109,50 +141,25 @@ const CommentArea = () => {
               <img className="bili-avatar-img" src={avatar} alt="Avatar" />
             </div>
           </div>
+          {/* content area */}
           <div className="reply-box-wrap">
             <textarea
               className="reply-box-textarea"
-              placeholder="Be Friendly"
+              placeholder="Send a friendly comment."
+              ref={inputRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
             />
             <div className="reply-box-send">
-              <div className="send-text">发布</div>
+              <div className="send-text" onClick={handlePublish}>
+                Publish
+              </div>
             </div>
           </div>
         </div>
         <div className="reply-list">
           {commentList.map((item) => (
-            <div key={item.rpid} className="reply-item">
-              <div className="root-reply-avatar">
-                <div className="bili-avatar">
-                  <img
-                    className="bili-avatar-img"
-                    alt=""
-                    src={item.user.avatar}
-                  />
-                </div>
-              </div>
-
-              <div className="content-wrap">
-                <div className="user-info">
-                  <div className="user-name">{item.user.uname}</div>
-                </div>
-                <div className="root-reply">
-                  <span className="reply-content">{item.content}</span>
-                  <div className="reply-info">
-                    <span className="reply-time">{item.ctime}</span>
-                    <span className="reply-time">like: {item.like}</span>
-                    {user.uid === item.user.uid && (
-                      <span
-                        className="delete-btn"
-                        onClick={() => handleDel(item.rpid)}
-                      >
-                        Delete
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <CommentItem item={item} onDel={handleDel} />
           ))}
         </div>
       </div>
