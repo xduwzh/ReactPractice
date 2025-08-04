70966d9e8a33e25c7e71034681f784fa671a123f
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-04 11:44:45 +0200

    new page

--- react-bill-test/src/components/Icon/index.js
+++ react-bill-test/src/components/Icon/index.js
@@ -1,7 +1,7 @@
-const Icon = () => {
+const Icon = (type) => {
   return (
     <img
-      src={`https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/reactbase/ka/food.svg`}
+      src={`https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/reactbase/ka/${type}.svg`}
       alt="icon"
       style={{
         width: 20,
