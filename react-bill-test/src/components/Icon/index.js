35f8ee6a42086a2e8eaaee94cfbcd01bf7bda3c7
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-04 11:34:39 +0200

    icon for diff use

---
+++ react-bill-test/src/components/Icon/index.js
@@ -0,0 +1,14 @@
+const Icon = () => {
+  return (
+    <img
+      src={`https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/reactbase/ka/food.svg`}
+      alt="icon"
+      style={{
+        width: 20,
+        height: 20,
+      }}
+    />
+  );
+};
+
+export default Icon;
--- react-bill-test/src/pages/Month/components/DayBill/index.js
+++ react-bill-test/src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import "./index.scss";
 import { useMemo, useState } from "react";
+import Icon from "@/components/Icon";
 
 const DailyBill = ({ date, billList }) => {
   const dayResult = useMemo(() => {
@@ -44,6 +45,7 @@ const DailyBill = ({ date, billList }) => {
         {billList.map((item) => {
           return (
             <div className="bill" key={item.id}>
+              <Icon type={item.useFor} />
               <div className="detail">
                 <div className="billType">{item.useFor}</div>
               </div>
