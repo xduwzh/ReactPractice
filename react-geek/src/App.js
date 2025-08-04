7f1d5b49ef6794fa02e42715503e7d2c70812216
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-04 13:27:01 +0200

    geek web proj setup, login page

---
+++ react-geek/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      this is app
+      <Button type="primary" />
+    </div>
+  );
+}
+
+export default App;
