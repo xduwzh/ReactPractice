2769cdcb79fec446326e1f82622856090528a636
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-06 22:49:45 +0200

    article list filter feature

--- react-geek/src/pages/Article/index.js
+++ react-geek/src/pages/Article/index.js
@@ -100,17 +100,41 @@ const Article = () => {
     },
   ];
 
+  // article filter feature
+  // params
+  const [reqData, setReqData] = useState({
+    status: "",
+    channel_id: "",
+    begin_pubdate: "",
+    end_pubdate: "",
+    page: 1,
+    per_page: 4,
+  });
+
   // get article list
   const [list, setList] = useState([]);
   const [count, setCount] = useState(0);
   useEffect(() => {
     async function getList() {
-      const res = await getArticleListAPI();
+      const res = await getArticleListAPI(reqData);
       setList(res.data.results);
       setCount(res.data.total_count);
     }
     getList();
-  }, []);
+  }, [reqData]);
+
+  // get data from form
+  const onFinish = (formValue) => {
+    // console.log(formValue);
+    // update params
+    setReqData({
+      ...reqData,
+      channel_id: formValue.channel_id,
+      status: formValue.status,
+      begin_pubdate: formValue.date[0].format("YYYY-MM-DD"),
+      end_pubdate: formValue.date[1].format("YYYY-MM-DD"),
+    });
+  };
 
   return (
     <div>
@@ -125,7 +149,7 @@ const Article = () => {
         }
         style={{ marginBottom: 20 }}
       >
-        <Form initialValues={{ status: "" }}>
+        <Form initialValues={{ status: "" }} onFinish={onFinish}>
           <Form.Item label="Status" name="status">
             <Radio.Group>
               <Radio value={""}>All</Radio>
