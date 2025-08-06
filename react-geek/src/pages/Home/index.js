3332719224cc94ca6c2a74516e64077d0a29d275
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-06 15:46:42 +0200

    created echart

--- react-geek/src/pages/Home/index.js
+++ react-geek/src/pages/Home/index.js
@@ -1,5 +1,38 @@
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const chartRef = useRef(null);
+  useEffect(() => {
+    const chartDom = chartRef.current;
+    // init chart object
+    const myChart = echarts.init(chartDom);
+    const option = {
+      xAxis: {
+        type: "category",
+        data: ["Vue", "React", "Angular"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: "bar",
+        },
+      ],
+    };
+    // render chart with option
+    option && myChart.setOption(option);
+  }, []);
+  return (
+    <div>
+      <div
+        ref={chartRef}
+        style={{ width: "500px", height: "400px", display: "block" }}
+      ></div>
+    </div>
+  );
 };
 
 export default Home;
