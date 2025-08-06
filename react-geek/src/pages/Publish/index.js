f03e50c9538d4913e06e44a0ce13c09b6e326a67
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-06 21:34:14 +0200

    limit image upload num

--- react-geek/src/pages/Publish/index.js
+++ react-geek/src/pages/Publish/index.js
@@ -123,6 +123,7 @@ const Publish = () => {
                 action={"http://geek.itheima.net/v1_0/upload"}
                 name="image"
                 onChange={onChange}
+                maxCount={imageType}
               >
                 <div style={{ marginTop: 8 }}>
                   <PlusOutlined />
